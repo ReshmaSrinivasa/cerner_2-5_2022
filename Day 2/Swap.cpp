1e8fd2964cc1134394da1de56f976dcb10fa85bf
Srinivasa, Reshma <Reshma.Srinivasa@cerner.com>
2022-09-16 10:21:36 +0530

    Day 2

---
+++ Day 2/Swap.cpp
@@ -0,0 +1,12 @@
+// cerner_2tothe5th_2022
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10, b = 20;
+cout<<a<<" "<<b<<endl;
+int temp = a;
+a = b;
+b = temp;
+cout<<a<<" "<<b<<endl;
+return 0;
+}
