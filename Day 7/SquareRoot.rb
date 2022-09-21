b4a225e5840c8c9da3b7662a7fe00a2467009b89
Reshma <113575882+ReshmaSrinivasa@users.noreply.github.com>
2022-09-21 10:26:34 +0530

    Day 7

---
+++ Day 7/SquareRoot.rb
@@ -0,0 +1,7 @@
+# cerner_2tothe5th_2022
+# Find square root of a number
+
+puts "Enter a number:"
+input = Integer(gets.chomp)
+result = Math.sqrt(input)
+puts "Squre root of #{input} is: #{result}"
