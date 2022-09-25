4cedc8dd4f00049354b9e905f2e723cb3dced3dc
Reshma <113575882+ReshmaSrinivasa@users.noreply.github.com>
2022-09-25 14:35:36 +0530

    Day 11

---
+++ Day 11/Factorial.r
@@ -0,0 +1,7 @@
+#cerner_2tothe5th_2022
+# R program to find the factorial value of given set of values using 
+
+factorial() method  
+factorialValue <- factorial(c(0, 1, 2, 3, 4, 8))  
+  
+print(factorialValue)  
