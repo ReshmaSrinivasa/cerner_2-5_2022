3bdf4a5e71fc9314c76f4e26a432eb6372545e43
Reshma <113575882+ReshmaSrinivasa@users.noreply.github.com>
2022-09-30 11:30:12 +0530

    Day 16

---
+++ Day 16/Sum.groovy
@@ -0,0 +1,11 @@
+/* cerner_2tothe5th_2022 */
+/* Code to find the sum of two numbers */
+
+class Sum {
+   static void main(String[] args) {
+      def x = 5;
+      def y = 4;
+
+      print 'Sum of x + y = ' + (x + y)
+   }
+}
