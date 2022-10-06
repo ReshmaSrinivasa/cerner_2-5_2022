fafb6f802e2d75a2891e385e8d3c467b66900d0a
Reshma <113575882+ReshmaSrinivasa@users.noreply.github.com>
2022-10-06 15:25:19 +0530

    Day 21

---
+++ Day 21/AddNumbers.jl
@@ -0,0 +1,8 @@
+#cerner_2tothe5th_2022
+#Julia code to add two numbers
+
+print("Enter number a: ")
+a = parse(Int, readline(stdin))
+print("Enter number b: ")
+b = parse(Int, readline(stdin))
+println("Sum: ", a + b)
